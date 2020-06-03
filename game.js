ff68ed9367177a89b09869eee527ef8f322445e5
Eitantan <56890550+Eitantan@users.noreply.github.com>
2020-06-03 14:27:44 -0600

    Create game.js

---
+++ game.js
@@ -0,0 +1,22 @@
+const config = {
+  width: 800,
+  height: 600,
+  backgroundColor: 0xadd8e6,
+
+  scene: {
+    preload,
+    create,
+    update
+  }
+}
+const game = new Phaser.Game(config)
+
+function preload() {
+
+}
+function create() {
+
+}
+function update() {
+  
+}
